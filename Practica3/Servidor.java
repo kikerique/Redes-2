f167765f2dc4373049431c4f1fc59b5ecdb64ce3
kikerique <enrique-romero_98@hotmail.com>
2020-03-19 20:11:15 -0600

    Practica 3 inicial

---
+++ Practica3/Servidor.java
@@ -0,0 +1,141 @@
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.lang.Math;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.InetSocketAddress;
+import java.net.InetAddress;
+import java.lang.Thread;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Servidor {
+    String dificultad="";
+    Socket  cliente; //Socket cliente
+    PrintStream  p; //Canal de escritura
+    BufferedReader  b; //Canal de Lectura
+    tablero tab=null,tab16=null;//Tableros para los dos tipos de juego
+
+    /*
+       void pideDificultad(Socket sc)
+        método que le solicita la dificultad a un cliente y crea un nuevo tablero si es necesario
+    */
+    public void pideDificultad(Socket sc){
+        try{
+            b = new BufferedReader ( new InputStreamReader  ( sc.getInputStream() ) );
+            p = new PrintStream  ( sc.getOutputStream() );
+            p.println("Bienvenido Usuario: " + sc.getLocalSocketAddress().toString());
+            p.println("Ingresa la dificultad\n1.-Principiante\n2.-Experto\nQ.-Salir");
+            //Leo lo que escribio el socket cliente en el canal de lectura
+            this.dificultad = b.readLine();
+            if(dificultad.equals("1")){
+                if(this.tab==null){
+                    this.tab=new tablero(dificultad);
+                    System.out.println("INICIANDO UN NUEVO JUEGO DE PRINCIPIANTES");
+                    System.out.println("Aqui estan las minas:\n"+Arrays.asList(tab.minas));
+                }
+            }
+            if(dificultad.equals("2")){
+                if(this.tab16==null){
+                    this.tab16=new tablero(dificultad);
+                    System.out.println("INICIANDO UN NUEVO JUEGO DE EXPERTOS");
+                    System.out.println("Aqui estan las minas:\n"+Arrays.asList(tab16.minas));
+                }
+            }
+            
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+        
+    }
+
+    /*
+       void iniciaServer(String HOST,int PORT,int numeroConexiones)
+        Método que crea un objeto de tipo Server Socket en la dirección ip y puertos especificados
+        y se queda a la espera de nuevas conexiones
+    */
+    
+    public void iniciaServer(String HOST,int PORT,int numeroConexiones){
+        ThreadGroup principiantes,expertos;
+        Thread hilo,hiloexp;
+        String respuesta="";
+        int conexionesActivasp=0,conexionesActivase=0;
+        Runnable nuevoCliente;
+        Scanner leer=new Scanner(System.in);
+        ServerSocket  s; //Socket servidor
+        Socket  sc; //Socket cliente
+        principiantes=new ThreadGroup("principiantes");
+        expertos=new ThreadGroup("expertos");
+        try {
+            //Creo el socket server
+            s = new ServerSocket (PORT,numeroConexiones,InetAddress.getByName(HOST));
+            
+            System.out.println("Servidor escuchando en la IP: "+s.getInetAddress().toString());
+            System.out.println("Servidor escuchando en el puerto: "+s.getLocalPort());
+            System.out.println("Principiantes iniciales:"+ principiantes.activeCount());
+            System.out.println("expertos iniciales: "+ expertos.activeCount());
+            while ( true ) {
+                //Invoco el metodo accept del socket servidor, me devuelve una referencia al socket cliente
+                sc = s.accept();
+                conexionesActivasp=principiantes.activeCount();
+                conexionesActivase=expertos.activeCount();
+                p = new PrintStream  ( sc.getOutputStream() );
+                if(conexionesActivasp==0){
+                    tab=null;
+                }
+                if(conexionesActivase==0){
+                    tab16=null;
+                }
+                pideDificultad(sc);    
+                if(dificultad.equals("1")){
+                    nuevoCliente = new hiloServidor(sc,dificultad,tab,principiantes,numeroConexiones);
+                    hilo=new Thread(principiantes,nuevoCliente);
+                    //System.out.println("principiantes: "+conexionesActivasp);
+                    if((conexionesActivasp)<numeroConexiones){
+                        hilo.start();
+                        //conexionesActivasp=principiantes.activeCount();
+                    }else{
+                        p.println("Servidor lleno");
+                        p.println("Hasta luego");
+                        sc.close();
+                    }
+                }
+                if(dificultad.equals("2")){
+                    nuevoCliente = new hiloServidor(sc,dificultad,tab16,expertos,numeroConexiones);
+                    hiloexp=new Thread(expertos,nuevoCliente);
+                    //System.out.println("expertos: "+conexionesActivase);
+                    if((conexionesActivase)<numeroConexiones){
+                        hiloexp.start();
+                        //conexionesActivase=expertos.activeCount();
+                    }else{
+                        p.println("Servidor lleno");
+                        p.println("Hasta luego");
+                        sc.close();
+                    }
+                }
+                    //hilo.start();
+                    //System.out.println("gg: " +nuevoCliente.activeCount());
+
+                    //conexionesActivas--; 
+            }
+            
+        } catch (IOException  e) {
+            System.out.println(e.toString());
+            System .out.println("No puedo crear el socket");
+        }
+    }
+
+
+    public static void main(String [] args) {
+        if(args.length<3){
+            System.out.println("Uso: java ejecutable HOST PORT #conexiones-permitidas");
+        }else{
+            Servidor s=new Servidor();
+            s.iniciaServer(args[0],Integer.parseInt(args[1]),Integer.parseInt(args[2]));
+        }
+    }
+
+}
---
+++ Practica3/hiloServidor.java
@@ -0,0 +1,195 @@
+import java.io.BufferedReader;
+import java.lang.Math;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.net.Socket;
+import java.time.LocalDateTime;
+import java.lang.Thread;
+
+public class hiloServidor implements Runnable {
+	String mensaje="",dificultad="";
+	PrintStream  p; //Canal de escritura
+    BufferedReader  b; //Canal de Lectura
+    Socket  cliente; //Socket cliente
+    LocalDateTime inicio,fin;
+    int casillas=0,numJugadores=0,jugadores=0;
+    tablero tab;
+    boolean bandera=false;
+    ThreadGroup grupo;
+	
+	public hiloServidor(Socket socket,String d,tablero t,ThreadGroup g, int j){
+		this.cliente=socket;
+		this.tab=t;
+		this.dificultad=d;
+        this.casillas=tab.casillas;
+        this.grupo=g;
+        this.numJugadores=j;
+        try{
+            this.b = new BufferedReader ( new InputStreamReader  ( cliente.getInputStream() ) );
+            this.p = new PrintStream  ( cliente.getOutputStream() );
+        }catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+	}
+	
+    
+    public void imprimeTablero(){
+        int ancho=0;
+        if(dificultad.equals("1")){
+            ancho=9;
+        }else{
+            ancho=16;
+        }
+        p.print("0 ");
+        for(int i=97;i<97+ancho;i++){
+            p.print(Character.toString((char)i)+"\t");
+        }
+        p.println();
+        for(int j=0;j<ancho;j++){
+            p.print("\n"+j+" ");
+            for(int i=97;i<97+ancho;i++){
+                p.print(tab.tablero.get(Character.toString((char) i)+Integer.toString(j))+"\t");
+            }
+            p.println();
+        }
+        //p.print(imprime);
+        //p.println();
+    }
+    public String validaJugada(String jugada){
+        if(this.tab.tablero.get(jugada)!=null){
+            if(this.tab.minas.get(jugada)==null){
+                if(this.tab.tablero.get(jugada).equals("D")){
+                    return "Escoge una casilla que no este desbloqueada,has perdido el turno";
+                }else{
+                    casillas=casillas-1;
+                    this.tab.setTablero(jugada,"D");
+                    if(casillas==0)
+                    {
+                        return "Has ganado el juego";
+                    }
+                    return "Desbloqueaste una casilla";
+                }
+            }else{
+                if(this.tab.tablero.get(jugada).equals("M")){
+                    return "Escoge una casilla que no este desbloqueada,has perdido el turno";
+                }else{
+                    this.tab.setTablero(jugada,"M");
+                    return "Explotaste una mina\nGame Over";
+                }
+                
+            }
+        }
+        return "Casilla Invalida";
+
+    }
+    public boolean checaJuegoTerminado(){
+        Iterator it = tab.tablero.values().iterator();
+        int min=0,vacios=0,desbloqueados=0;
+        while (it.hasNext()) {
+            String valor = it.next().toString();
+            if(valor.equals("M")){
+                min++;
+            }
+            if(valor.equals("V")){
+                vacios++;
+            }
+            //it.remove(); // avoids a ConcurrentModificationException
+        }
+        if(tab.dificultad.equals("1")){
+            if(min==10){
+                p.println("Todas las minas han sido explotadas\nJuego Terminado");
+                return true;
+            }
+            if(vacios==min){
+                p.println("Todas las casillas han sido desbloqueadas\nJuego Terminado");
+                return true;
+            }
+            return false;
+        }
+        if(tab.dificultad.equals("2")){
+            if(min==40){
+                p.println("Todas las minas han sido explotadas\nJuego Terminado");
+                return true;
+            }
+            if(vacios==min){
+                p.println("Todas las casillas han sido desbloqueadas\nJuego Terminado");
+                return true;
+            }
+            return false;
+        }
+        return false;
+    }
+	public void run(){
+        //tab.addObserver(this);
+        //imprimeTablero(tab.getTablero().toString());
+        p.println("Bienvenido Espera tu turno\n");
+        jugadores=grupo.activeCount();
+        try{
+        	inicio = LocalDateTime.now();
+            while (true){
+                synchronized(tab.tablero){
+                    if(grupo.activeCount()<numJugadores && bandera==false){
+                        p.println("Esperando a todos los jugadores");
+                        p.println("Jugadores actualmente conectados: "+grupo.activeCount());
+                        p.println("Jugadores necesarios para el inicio del juego: "+numJugadores);
+                        tab.tablero.wait();
+                        tab.tablero.notify();
+                    }
+                    if(grupo.activeCount()==numJugadores && numJugadores>1){
+                        System.out.println("Ahora si");
+                        bandera=true;
+                        tab.tablero.notify();
+                        tab.tablero.wait();
+                    }
+                    //tab.tablero.notify();
+                    if(checaJuegoTerminado()){
+                        tab.tablero.notify();
+                        break;
+                    }
+                    imprimeTablero();
+                	p.println("Ingresa la casilla que quieres jugar (ejemplo a0): ");
+                	mensaje=b.readLine();
+                	if (mensaje.equals("by")) {
+                    	fin=LocalDateTime.now();
+                    	p.println("Duración de la partida (HH:MM:SS): "+(fin.getHour()-inicio.getHour())+":"+(fin.getMinute()-inicio.getMinute())+":"+(fin.getSecond()-inicio.getSecond()));
+                        tab.tablero.notify();
+                        p.println("Hasta luego");
+                    	break;
+                    }else{
+                	    mensaje=validaJugada(mensaje);
+                	    if(mensaje.equals("Explotaste una mina\nGame Over") || mensaje.equals("Has ganado el juego"))
+                	    {
+                            fin=LocalDateTime.now();
+                            p.println("Duración de la partida (HH:MM:SS): "+(fin.getHour()-inicio.getHour())+":"+(fin.getMinute()-inicio.getMinute())+":"+(fin.getSecond()-inicio.getSecond()));
+                    	    p.println(mensaje);                                
+                            tab.tablero.notify();
+                            p.println("Hasta luego");
+                            break;
+                	    }
+                	    p.println(mensaje);
+                        tab.tablero.notify();
+                            //System.out.println("Esto hay activo:"+this.activeCount());
+                	}
+                    if(jugadores>1){
+                        tab.tablero.wait();
+                        jugadores=grupo.activeCount();
+                    }   
+                }
+                    //break;
+            }
+        	p.close();
+        	b.close();
+        	this.cliente.close();
+        }catch(InterruptedException e){
+            System.out.println(e.toString());
+        }catch(IOException e){
+            System.out.println(e.toString());
+            System .out.println("No puedo crear el socket");
+        }
+	}
+
+}
